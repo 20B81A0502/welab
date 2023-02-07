5f5344248c15fd6b8f2eb54257ed89733b67be5b
20B81A0502 <chinthaabhinov@gmail.com>
2023-02-07 12:10:27 +0530

    commited

--- we502/exp2/script.js
+++ we502/exp2/script.js
@@ -1,14 +1,14 @@
 function getdetails(){
     let xhttp = new XMLHttpRequest();
-    let div document.getElementById("userData");
+    let div = document.getElementById("userData");
     xhttp.onreadystatechange=function(){
         if(this.status == 200 && this.readystate==4){
             let data = JSON.parse(xhttp.responseText);
             data.forEach(ele =>{
                 console.log(ele.name + " :: "+ele.email);
-                div.innerHTML +=   `<b>$(ele.name)</b>`
+                div.innerHTML +=   `<b>$(ele.name)</b>
                 Email: ${ele.email}
-                <br>
+                <br> `
             });
         }
     }
